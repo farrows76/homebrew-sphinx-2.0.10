d3b5ba2f7f15563a1f315e0897fa46ad1b0eeb37
Steve Farrow <sfarrow@BandsinownsMBP3.lan>
2017-04-21 12:52:36 -0700

    removed comments

--- sphinx@2.0.10.rb
+++ sphinx@2.0.10.rb
@@ -1,7 +1,3 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class SphinxAT2010 < Formula
   desc "Full-text search engine"
   homepage "http://www.sphinxsearch.com"
@@ -14,26 +10,14 @@ class SphinxAT2010 < Formula
   depends_on :mysql if build.include? 'mysql'
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
     system "./configure", "--with-mysql",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test sphinx@2.0.10`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
